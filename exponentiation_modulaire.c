57f29b0926c9d5f6f874c08eff61cc6c8f2cd64f
Youssef <ybkstudies@gmail.com>
2020-11-26 22:37:20 +0100

    exponentiation modulaire faite

--- exponentiation_modulaire.c
+++ exponentiation_modulaire.c
@@ -3,21 +3,44 @@
 #include <math.h>
 #include <gmp.h>
 
-int exp_mod(mpz_t g, mpz_t exp, mpz_t mod)
+void exp_mod(mpz_t result, mpz_t base, mpz_t exp, mpz_t mod)
 {
-  mpz_t result = 1;
-  while (exp != 0)
+  // mpz_init(result);
+  // mpz_set_ui(result,1)
+  mpz_t base_copy, exp_copy;
+  mpz_init_set(base_copy, base);
+  mpz_init_set(exp_copy, exp);
+  mpz_init_set_ui(result, 1);
+  while (mpz_cmp_d(exp_copy, 0))
   {
-    if (exp & 1)
-      result = (result * g) % mod;
-    g *= g % mod;
-    exp >>= 1 ;
+    // if (exp & 1)
+    // mpz_and()
+    // flag = mpz_cmp_d(exp,1);
+    if (mpz_tstbit(exp_copy, 0))
+    {
+      // result = (result * g) % mod;
+      mpz_mul(result, result, base_copy);
+      mpz_mod(result,result,mod);
+    }
+    // g *= g % mod;
+    mpz_mul(base_copy, base_copy, base_copy);
+    mpz_mod(base_copy, base_copy, mod);
+    // exp >>= 1 ;
+    mpz_fdiv_q_ui(exp_copy, exp_copy, 2);
+
   }
-  return result;
+  mpz_clears(base_copy, exp_copy, NULL);
 }
+
 int main(){
-  double a = 10;
-  double q =  pow(5,47);
-  q = fmod(q,10);
-  printf("%d %f\n", exp_mod(5,47,10), q);
+  mpz_t a, b, mod, result;
+  mpz_init_set_ui(a,84684654546);
+  mpz_init_set_ui(b,54564456465465);
+  mpz_init_set_ui(mod,8797);
+  mpz_inits(result);
+  mpz_powm(result, a, b, mod);
+  gmp_printf ("%Zd exp %Zd mod %Zd %Zd\n", a, b, mod, result);
+  exp_mod(result, a, b, mod);
+  gmp_printf ("%Zd exp %Zd mod %Zd %Zd\n", a, b, mod, result);
+
 }
